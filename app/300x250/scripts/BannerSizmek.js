55d5189fb0549d498566384953c618ffa9f37914
tim.hennessey <Tim.Hennessey@deutschinc.com>
2017-09-06 14:25:10 -0700

    initial commit

---
+++ app/300x250/scripts/BannerSizmek.js
@@ -0,0 +1,41 @@
+var app = app || {}; 
+
+
+app.Banner = (function () {    
+
+    // --------------------------------------------------------------------------------------
+    // check to see if EB has initialized
+    function initialize() {
+        if (!EB.isInitialized()) {
+            EB.addEventListener(
+                EBG.EventName.EB_INITIALIZED, 
+                handleEBInit
+            );
+        } 
+        else {
+            handleEBInit();
+        }
+    }
+
+    // --------------------------------------------------------------------------------------
+    // Runs when EB is ready.
+    function handleEBInit() {
+        var event = document.createEvent('Event');
+        event.initEvent('READY', true, true);
+        document.dispatchEvent(event);
+
+        document.getElementById('button-exit').addEventListener('click', handleExit, false);
+    }
+
+
+    // --------------------------------------------------------------------------------------
+    function handleExit(e) {
+        EB.clickthrough();
+    }
+
+    // --------------------------------------------------------------------------------------
+    // Publicly accessible methods and properties
+    return { 
+        initialize:initialize    
+    }
+})();
